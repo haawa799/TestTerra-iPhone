f0fa948ebec236f49cb6345cee62cc1e478ce4ba
Andrew <haawaplus@gmail.com>
2013-06-13 10:11:49 +0300

    Initial Commit

---
+++ TestTerra/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestTerra
+//
+//  Created by Andrew Kharchyshyn on 6/13/13.
+//  Copyright (c) 2013 kharchyshyn.andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestTerraTests/TestTerraTests.m
@@ -0,0 +1,36 @@
+//
+//  TestTerraTests.m
+//  TestTerraTests
+//
+//  Created by Andrew Kharchyshyn on 6/13/13.
+//  Copyright (c) 2013 kharchyshyn.andrew. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestTerraTests : XCTestCase
+
+@end
+
+@implementation TestTerraTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
